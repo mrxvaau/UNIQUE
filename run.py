5cbdef2ab80755c80057d8b064c3c4521f68da1a
Mrx™ <eyblish@gmail.com>
2022-08-17 18:08:30 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
 import os
+try:
+    import requests
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n')
+    os.system('pip install requests')
+
+try:
+    import bs4
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n')
+    os.system('pip install bs4')
+
+try:
+    import rich
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
+    os.system('pip install rich')
+#################################################################################
 import ru
-os.system("git pull")
-ru.menu_apikey()
+
+if __name__ == '__main__':
+    os.system("git pull");os.system("pkg install play-audio")
+    ru.menu_apikey()
+
